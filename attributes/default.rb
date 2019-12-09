451cf75ce54e0de4e8804da854d894679a706192
Mahmoud Ismail <mahmoud@logicalclocks.com>
2019-12-09 09:03:00 +0100

    use test path for deploying epipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@ include_attribute "elasticsearch"
 default['epipe']['version']                  = "0.12.0"
 default['epipe']['user']                     = node['install']['user'].empty? ? node['hops']['hdfs']['user'] : node['install']['user']
 default['epipe']['group']                    = node['install']['user'].empty? ? node['hops']['group'] : node['install']['user']
-default['epipe']['url']                      = "#{node['download_url']}/epipe/#{node['platform_family']}/epipe-#{node['epipe']['version']}.tar.gz"
+default['epipe']['url']                      = "#{node['download_url']}/epipe/test/#{node['platform_family']}/epipe-#{node['epipe']['version']}.tar.gz"
 default['epipe']['systemd']                  = "true"
 default['epipe']['dir']                      = node['install']['dir'].empty? ? "/srv" : node['install']['dir']
 default['epipe']['home']                     = node['epipe']['dir'] + "/epipe-" + "#{node['epipe']['version']}"
